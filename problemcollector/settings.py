cce651fcb3e52b6e7aaa96eab460bc67799092da
emilyyejia <EMILYYEJIA@GMAIL.COM>
2025-07-23 11:57:38 -0400

    Config deployment

--- problemcollector/settings.py
+++ problemcollector/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Other settings above
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals(), databases=False)
